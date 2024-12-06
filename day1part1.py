ea17cb42322f49376b4b2c9b32602f95511fc2a8
CodyG <codygeorge315@gmail.com>
2024-12-06 12:59:15 -0600

    output 2 lists sorted as int type

--- day1part1.py
+++ day1part1.py
@@ -1,1 +1,22 @@
-print("Hello World")
+# open the file and read the lines
+def read_numbers_from_file(list1, list2):
+    with open("testinput.txt", "r") as file:
+        for line in file:
+            x, y = line.strip().split()
+            list1.append(int(x))
+            list2.append(int(y))
+
+    list1.sort()
+    list2.sort()
+
+
+def main():
+    list1 = []
+    list2 = []
+    read_numbers_from_file(list1, list2)
+    # manipulate the list
+    print(list1, list2)
+
+
+if __name__ == "__main__":
+    main()
