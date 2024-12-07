4463fd70687d9555d40d578952934efd7a9bbc7c
CodyG <codygeorge315@gmail.com>
2024-12-07 01:23:50 -0600

    Part 1 Day 1 Completed

--- day1part1.py
+++ day1part1.py
@@ -1,22 +1,28 @@
 # open the file and read the lines
 def read_numbers_from_file(list1, list2):
-    with open("testinput.txt", "r") as file:
+    with open("input.txt", "r") as file:
         for line in file:
             x, y = line.strip().split()
             list1.append(int(x))
             list2.append(int(y))
-
+    # sort each list in ascending order
     list1.sort()
     list2.sort()
 
-
+# define main function
 def main():
+    total = 0 
     list1 = []
     list2 = []
     read_numbers_from_file(list1, list2)
+
     # manipulate the list
-    print(list1, list2)
+    for i, j in zip(list1, list2):
+       difference = i - j
+       absval = abs(difference)
+       total += absval
+    else:
+        print(total)
 
-
-if __name__ == "__main__":
-    main()
+# call main function
+main()
