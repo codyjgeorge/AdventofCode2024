6a4181ad3f20e1e9c674a20c3dd87f602facdce1
CodyG <codygeorge315@gmail.com>
2024-12-14 16:05:11 -0600

    star 3 complete

--- day_2/star3.py
+++ day_2/star3.py
@@ -1,5 +1,5 @@
 def read_numbers_from_file(matrix):
-    with open("testinput.txt", "r") as file:
+    with open("input.txt", "r") as file:
         for line in file:
             report = line.strip().split()
             matrix.append(report)
@@ -7,17 +7,35 @@ def read_numbers_from_file(matrix):
 
 def main():
     matrix = []
-    issafe = True
+    is_Safe = False
+    unsafe = 0
+    safe = 0
     read_numbers_from_file(matrix)
 
     for line in matrix:
         report = line
-        report = [int(i) for i in report]   
-        print(report)
-        for i in report:
-            if report[i] < report[i+1]:
-                print("increasing")
-            else:
-                print("unsafe")
+        report = [int(i) for i in report] # changes each string number in report to int
+
+        # checks if list is ascending or descneding
+        if sorted(report) == report or sorted(report, reverse = True) == report:
+            is_Safe = True
+            while is_Safe == True:
+                # search if sorted(safe) list is still safe by step
+                for i in range(len(report) - 1):
+                    difference = report[i] - report[i +1]
+                    step = abs(difference) # finds step interval to adjacent number
+                    if step < 1 or step > 3:
+                        is_Safe = False
+                        unsafe += 1
+                        
+                if is_Safe == True:
+                    safe += 1
+                    is_Safe = False
+
+        else:
+            unsafe += 1
         report = []
+
+    print("Safe: ", safe)
+    print("Unsafe: ", unsafe)
 main()
